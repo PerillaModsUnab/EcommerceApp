56a35552b44732cb8e5c97f5d9ab67f20f6d293d
juanperilla <jperilla@unab.edu.co>
2025-04-09 09:02:17 -0500

    LoginScreen

--- app/src/main/java/LoginScreen.kt
+++ app/src/main/java/LoginScreen.kt
@@ -1,5 +1,6 @@
 import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,15 +11,20 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Button
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -33,23 +39,29 @@ fun LoginScreen() {
             Modifier
                 .padding(innerPadding)
                 .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+
+
         ) {
+
             Image(
                 painter = painterResource(R.drawable.logounab),
                 contentDescription = "Logo Unab",
                 modifier = Modifier.size(150.dp)
             )
             Spacer(modifier = Modifier.height(32.dp))
+
+
             Text(
                 text = "Iniciar sesión",
-                fontSize = 24.sp, // Corrected fontSize instead of fontsize
-                color = Color(0xFFFF9900), // Fixed color hex value
+                fontSize = 24.sp,
+                color = Color(0xFFFF9900),
                 fontWeight = FontWeight.Bold
             )
-            Spacer(modifier = Modifier.height(16.dp)) // Added space between title and input field
+            Spacer(modifier = Modifier.height(16.dp))
 
-            // OutlinedTextField
+
             OutlinedTextField(
                 value = "",
                 onValueChange = {},
@@ -58,7 +70,7 @@ fun LoginScreen() {
                     Icon(
                         imageVector = Icons.Default.Email,
                         contentDescription = "Email",
-                        tint = Color(0xFFF9900)
+                        tint = Color(0xFFFF9900)
                     )
                 },
                 label = {
@@ -66,6 +78,49 @@ fun LoginScreen() {
                 },
                 shape = RoundedCornerShape(12.dp)
             )
+            Spacer(modifier = Modifier.height(16.dp))
+
+
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                modifier = Modifier.fillMaxWidth(),
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Lock,
+                        contentDescription = "Contraseña",
+                        tint = Color(0xFFFF9900)
+                    )
+                },
+                label = {
+                    Text(text = "Contraseña")
+                },
+                shape = RoundedCornerShape(12.dp)
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+
+            Button(
+                onClick = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp),
+                shape = RoundedCornerShape(12.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFFFF9900)
+                )
+            ) {
+                Text("Iniciar sesión")
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+
+
+            TextButton(onClick = {}) {
+                Text(
+                    text = "¿No tienes una cuenta? Regístrate",
+                    color = Color(0xFFFF9900)
+                )
+            }
         }
     }
 }
